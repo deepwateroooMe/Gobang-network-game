c48e15c6da395ff0390c805a330b0927fbbcb913
Nanxing Xiao <506830915@qq.com>
2017-06-05 20:05:14 +0800

    2017-6-5

--- GobangClient/GobangClient/ControlHander.cs
+++ GobangClient/GobangClient/ControlHander.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
+﻿using System.Windows.Forms;
 
 namespace GobangClient
 {
